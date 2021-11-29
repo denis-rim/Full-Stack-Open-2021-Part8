8900ef28820dea03d100a480346436e76bd82b9d
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-11-29 17:02:47 +0200

    8.8: Authors view

---
+++ library/client/src/components/Authors.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Authors = (props) => {
+  if (!props.show) {
+    return null;
+  }
+  if (!props.authors) {
+    return <div>Loading...</div>;
+  }
+
+  const authors = [...props.authors];
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Authors;
